2b5505eda692b7b21974d3cc8148a69f0184cc0e
Jarek Stelmach <j.stelmach@onet.eu>
2024-10-12 23:17:55 +0200

    Added code to ex 5

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,50 +1,81 @@
 import React from 'react';
+import { Route, useParams, useHistory } from "react-router-dom"
+import Shop from "../src/components/Shop"
+import products from "./../src/products.json"
+
+const SearchPage = () => {
+
+    const {minPrice, maxPrice, searchTerm} = useParams()
+    const minPriceNumber = Number(minPrice)
+    const productsAfterMinPrice = products.filter(p => p.price >= Number(minPrice))
+
+    return <p>{minPrice}, {maxPrice}, {searchTerm}</p>
+
+}
 
 const Task05 = () => {
+	const initSearch = {
+		text: "",
+		priceMin: "",
+		priceMax: "",
+	}
 
-const initSearch = {
-    text: '',
-    priceMin: '',
-    priceMax: '',
-}
+	const [form, setForm] = useState(initSearch)
+    const history = useHistory()
 
-const [form, setForm] = useState(initSearch)
+	function handleChange(e) {
+		const value = e.target.value
+		const name = e.target.name
+		setForm(state => {
+			return {
+				...state,
+				[name]: value,
+			}
+		})
+	}
 
-function handleChange(e) {
-    const value = e.target.value
-    const name = e.target.value
-    setForm((state) => {
-      return {
-        ...state,    
-        [name]: value, 
-      }   
-    })
-}
+	function handleSubmit(e) {
+        e.preventDefault()
 
-function handleSubmit () {
+        const { priceMin, priceMax, text } = form
 
-}
+        history.push(`/task05/${priceMin},${priceMax}-${text}`)
+    }
 
-    return (
-        <>
-            <h1>Task05</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Fraza</label>
-                    <input name="text" value={form.text} onChange={handleChange}/> 
-                </div>
-                <div>
-                    <label>Cena minimalna</label>
-                    <input name="priceMin" value={form.priceMin} /> 
-                </div>
-                <div>
-                    <label>Cena maksymalna</label>
-                    <input name="priceMax" value={form.priceMax} /> 
-                </div>
-                <div><input type="submit"/></div>
-            </form>
-        </>
-    );
+	return (
+		<>
+			<h1>Task05</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label>Fraza</label>
+					<input name='text' value={form.text} onChange={handleChange} />
+				</div>
+				<div>
+					<label>Cena minimalna</label>
+					<input
+						name='priceMin'
+						value={form.priceMin}
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<label>Cena maksymalna</label>
+					<input
+						name='priceMax'
+						value={form.priceMax}
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<input type='submit' />
+				</div>
+			</form>
+
+			<Route path='/task05/:minPrice?,:maxPrice?-:searchTerm?'>
+				<SearchPage />
+			</Route>
+		</>
+	)
 }
 
 export default Task05;
